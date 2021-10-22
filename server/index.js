864853080eb548abf62a7e46d6b68794209e66d6
zzero0622 <zzero9158@gmail.com>
2021-10-22 20:26:09 +0900

    test auto-deploy3

--- server/index.js
+++ server/index.js
@@ -3,4 +3,4 @@ const server = require('http').createServer(app);
 app.get('/', (req, res) => {
   res.status(200).send('hello maat-bab');
 });
-server.listen(4000, () => console.log('port 4000'));
+server.listen(4001, () => console.log('port 4001'));
