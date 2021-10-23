d12ad610f6918c2981b452afb76d39c6614c7394
zzero0622 <zzero9158@gmail.com>
2021-10-24 04:05:42 +0900

    set11

--- server/index.js
+++ server/index.js
@@ -6,11 +6,6 @@ const router = require('./routes');
 app.use('/', router);
 
 const PORT = process.env.PORT;
-const TEST = process.env.TEST;
-const TEST1 = process.env.TEST1;
-console.log(TEST);
 server.listen(PORT, () => {
-  console.log('test : ', TEST);
-  console.log('test1 : ', TEST1);
   console.log(`Dev-Child server is running at ${PORT} port`);
 });
