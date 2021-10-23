01a85f1afbe4b66950c2a87d954d8aeeda5e5503
seonwoong <lso5936@gmail.com>
2021-10-23 20:14:45 +0900

    nav.js Done

--- client/src/Component/Nav.js
+++ client/src/Component/Nav.js
@@ -1,21 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Nav.css';
 import { useHistory } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHamburger } from '@fortawesome/free-solid-svg-icons'
 
 function Nav({isLogin, logoutHandler}) {
-    const history = useHistory()
+    const history = useHistory();
+
+    const [toggle, setToggle] = useState(false)
 
     const MoveAbout = () => {
         history.push('/')
-    }
-    const MoveMain = () => {
-        history.push('/main')
-    }
-    const MoveChat = () => {
-        history.push('/chat')
-    }
-    const MoveMakeMeet = () => {
-        history.push('/makemeet')
     }
     const MoveEditInfo = () => {
         history.push('/editinfo')
@@ -26,29 +21,38 @@ function Nav({isLogin, logoutHandler}) {
     const MoveLogin = () => {
         history.push('/login')
     }
+    const toggleHandler = () => {
+        setToggle(!toggle)
+    }
     return (
         <div>
-            <div className='navigation__bar'>
-               <div className='navigation__logo' onClick={MoveAbout}></div>
-                <div className='navigation__menu__list'>
-                    <span className='navigation__menu' onClick={MoveMain}>
-                        맞밥 약속 조회하기
-                    </span>
-                    <span className='navigation__menu' onClick={MoveChat}>
-                     나의 맞밥 약속 보기
-                    </span>
-                    <span className='navigation__menu' onClick={MoveMakeMeet}>
-                     맞밥 약속 만들기
-                    </span>
-                </div>
-                <div className='navigation__bar__buttons'>
-                    {isLogin ? <button className='navigation__button' onClick={MoveEditInfo}>마이페이지</button> :
-                    <button className='navigation__button' onClick={MoveSignUp}>회원가입</button>}
-                    {isLogin ? <button className='navigation__button' onClick={logoutHandler}>로그아웃</button> :
-                    <button className='navigation__button' onClick={MoveLogin}>로그인</button>}
-                </div>
-            
-            </div>
+            <nav className='navigation__bar'>
+               <div className='navigation__logo' onClick={MoveAbout}>
+                   
+               </div>
+                <ul className={toggle ? 'navigation__menu__list action' : 'navigation__menu__list'}>
+                    <li className='navigation__menu'>
+                        <a href='/main'>맞밥 약속 조회하기</a>
+                        
+                    </li>
+                    <li className='navigation__menu'>
+                     <a href='/chat'>나의 맞밥 약속보기</a>
+                    </li>
+                    <li className='navigation__menu'>
+                     <a href='/makemeet'>맞밥 약속 만들기</a>
+                    </li>
+                </ul>
+                <ul className={toggle ? 'navigation__bar__buttons action' : 'navigation__bar__buttons'}>
+                    {isLogin ? <li><button className='navigation__button' onClick={MoveEditInfo}>마이페이지</button></li> :
+                    <li><button className='navigation__button' onClick={MoveSignUp}>회원가입</button></li>}
+                    {isLogin ? <li><button className='navigation__button' onClick={logoutHandler}>로그아웃</button></li> :
+                   <li><button className='navigation__button' onClick={MoveLogin}>로그인</button></li>}
+                </ul>
+
+                <a href='#' className='navigation__toggle' onClick={toggleHandler}>
+                <FontAwesomeIcon icon={faHamburger}/>
+                    </a>
+            </nav>
         </div>
     )
 }
