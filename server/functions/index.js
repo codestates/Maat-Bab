a9e543f075fe73cc02d74bc0173bf0831717b371
zzero0622 <zzero9158@gmail.com>
2021-10-25 15:45:22 +0900

    auth function

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,1 +1,17 @@
-module.exports = {};
+const { isAuthorized, checkRefeshToken, generateAccessToken, sendAccessToken } = require('../tokenFunctions');
+
+module.exports = {
+  isAuth: (req, res) => {
+    const userinfo = checkRefeshToken(req);
+    if (userinfo) {
+      if (isAuthorized(req)) {
+        return true;
+      }
+      const accessToken = generateAccessToken(userinfo);
+      sendAccessToken(res, accessToken);
+      return true;
+    } else {
+      return false;
+    }
+  },
+};
