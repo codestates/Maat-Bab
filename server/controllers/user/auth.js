d9f9b99c1bce499bc0ed33e3669849a67f2a68f2
zzero0622 <zzero9158@gmail.com>
2021-10-25 16:18:49 +0900

    auth.js

--- server/controllers/user/auth.js
+++ server/controllers/user/auth.js
@@ -1,3 +1,13 @@
+const { isAuth } = require('../../functions');
+const { isAuthorized } = require('../tokenFunctions');
+
 module.exports = {
-  get: (req, res) => {},
+  get: (req, res) => {
+    if (isAuth(req, res)) {
+      const userinfo = isAuthorized(req);
+      return res.status(200).send(userinfo);
+    }
+
+    return res.status(400).send('failed to authenticate');
+  },
 };
--- server/functions/index.js
+++ server/functions/index.js
@@ -1,17 +1,22 @@
-const { isAuthorized, checkRefeshToken, generateAccessToken, sendAccessToken } = require('../tokenFunctions');
+const {
+  isAuthorized,
+  checkRefeshToken,
+  generateAccessToken,
+  sendAccessToken,
+} = require('../controllers/tokenFunctions');
 
 module.exports = {
   isAuth: (req, res) => {
     const userinfo = checkRefeshToken(req);
-    if (userinfo) {
+    if (!userinfo) {
+      return false;
+    } else {
       if (isAuthorized(req)) {
         return true;
       }
       const accessToken = generateAccessToken(userinfo);
       sendAccessToken(res, accessToken);
       return true;
-    } else {
-      return false;
     }
   },
 };
