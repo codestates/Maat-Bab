4ed5d7989b2d81bd00558190a24693ff3fd57a03
zzero0622 <zzero9158@gmail.com>
2021-10-25 15:24:57 +0900

    confirm package.json

--- server/controllers/user/userinfo.js
+++ server/controllers/user/userinfo.js
@@ -1,10 +1,10 @@
-// const { User, Taste, User_taste } = require('../../models');
+const { User, Taste, User_taste } = require('../../models');
 
-// const {
-//   isAuthorized,
-//   checkRefeshToken,
-//   generateAccessToken,
-// } = require('../tokenFunctions');
+const {
+  isAuthorized,
+  checkRefeshToken,
+  generateAccessToken,
+} = require('../tokenFunctions');
 module.exports = {
   get: (req, res) => {
     console.log('userinfo get');
@@ -14,10 +14,50 @@ module.exports = {
   taste: {
     user_id: {
       get: (req, res) => {
-        console.log('taste user_id get');
+        const { user_id } = req.params;
+        User_taste.findAll({
+          where: { user_id },
+          include: { model: Taste },
+        })
+          .then((data) => {
+            if (!data.length) return res.status(404).send('No contents');
+            const tastes = data.map((user_taste) => {
+              return user_taste.Taste;
+            });
+            return res.status(200).send(tastes);
+          })
+          .catch((err) => {
+            console.log(err);
+            return res.status(500).send('Internal Server Error');
+          });
       },
-      patch: (req, res) => {
-        console.log('taste user_id patch');
+      patch: async (req, res) => {
+        const { user_id } = req.params;
+        await User_taste.destroy({ where: { user_id } }).catch((err) => {
+          console.log(err);
+          return res.status(500).send('Internal Server Error');
+        });
+
+        if (!req.body.taste_id) return res.status(200).send([]);
+        const tastes = req.body.taste_id;
+        for (const taste_id of tastes) {
+          await User_taste.create({ user_id, taste_id });
+        }
+
+        await User_taste.findAll({
+          where: { user_id },
+          include: { model: Taste },
+        })
+          .then((data) => {
+            const tastes = data.map((user_taste) => {
+              return user_taste.Taste;
+            });
+            return res.status(200).send(tastes);
+          })
+          .catch((err) => {
+            console.log(err);
+            return res.status(500).send('Internal Server Error');
+          });
       },
     },
   },
