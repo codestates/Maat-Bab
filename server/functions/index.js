e3dbb23c14f5e4d45aa2cfd2f68ab75a31f1dd59
zzero0622 <zzero9158@gmail.com>
2021-10-26 11:15:00 +0900

    edit patch userinfo

--- server/functions/index.js
+++ server/functions/index.js
@@ -9,8 +9,8 @@ module.exports = {
   isAuth: (req, res) => {
     let userinfo = isAuthorized(req);
     if (!userinfo) {
-      if (checkRefeshToken(req)) {
-        userinfo = checkRefeshToken(req);
+      userinfo = checkRefeshToken(req);
+      if (userinfo) {
         res.clearCookie('accessToken');
         const accesstoken = generateAccessToken(userinfo);
         sendAccessToken(res, accesstoken);
