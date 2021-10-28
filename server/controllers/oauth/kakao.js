cf49487e589068005837bd18678739d115dce489
zzero0622 <zzero9158@gmail.com>
2021-10-28 11:34:07 +0900

    delete userinfo path parameter

--- server/controllers/oauth/kakao.js
+++ server/controllers/oauth/kakao.js
@@ -17,3 +17,9 @@ module.exports = {
     get: (req, res) => {},
   },
 };
+
+// *client_id: String = 앱 REST API 키 3d5f1fdf916d372e883f1a5a9b993c5f
+// *redirect_uri: String = 인가 코드가 리다이렉트 될 URI
+// *response_type: String = code 로 고정
+// state: String = 로그인 요청과 콜백 간에 상태를 유지하기 위해 사용되는 임의의 문자열(정해진 형식 없음)
+//                 Cross-Site Request Forgery(CSRF) 공격으로부터 보호하기 위해 해당 파라미터 사용을 권장함
