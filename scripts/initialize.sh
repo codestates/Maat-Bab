4ab23f62808c6e9b415ed7c0848d35c386cc915f
zzero0622 <zzero9158@gmail.com>
2021-10-29 18:20:30 +0900

    https24

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -7,8 +7,8 @@ sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
 sudo chown ubuntu /etc/authbind/byport/80
 sudo chmod 755 /etc/authbind/byport/80
-sudo touch /etc/authbind/byport/443
-sudo chown ubuntu /etc/authbind/byport/443
-sudo chmod 755 /etc/authbind/byport/443
+# sudo touch /etc/authbind/byport/443
+# sudo chown ubuntu /etc/authbind/byport/443
+# sudo chmod 755 /etc/authbind/byport/443
 sudo chmod -R 755 /etc/letsencrypt/archive
 sudo chmod -R 755 /etc/letsencrypt/live
--- server/index.js
+++ server/index.js
@@ -8,7 +8,6 @@ const fs = require('fs');
 
 const key = fs.readFileSync('/etc/letsencrypt/live/server.maat-bab.com/privkey.pem', 'utf8');
 const cert = fs.readFileSync('/etc/letsencrypt/live/server.maat-bab.com/cert.pem', 'utf8');
-// const ca = fs.readFileSync('/etc/letsencrypt/live/server.maat-bab.com/chain.pem', 'utf8');
 const ca = fs.readFileSync('/etc/letsencrypt/live/server.maat-bab.com/fullchain.pem', 'utf8');
 
 const credentials = {
@@ -41,44 +40,3 @@ http_server.listen(HTTP_PORT, () => {
 https_server.listen(HTTPS_PORT, () => {
   console.log(`Dev-Child server is running at ${HTTPS_PORT} port`);
 });
-
-// const fs = require('fs');
-// const http = require('http');
-// const https = require('https');
-// const express = require('express');
-
-// const app = express();
-// app.use(express.static('public'));
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin: true,
-//     methods: ['GET', 'POST', 'OPTIONS', 'PATCH', 'DELETE'],
-//     credentials: true,
-//   })
-// );
-
-// // const key = fs.readFileSync(__dirname + '/key.pem', 'utf8');
-// // const cert = fs.readFileSync(__dirname + '/cert.pem', 'utf8');
-// const key = fs.readFileSync('/etc/letsencrypt/live/server.maat-bab.com/privkey.pem', 'utf8');
-// const cert = fs.readFileSync('/etc/letsencrypt/live/server.maat-bab.com/cert.pem', 'utf8');
-// // const ca = fs.readFileSync('/etc/letsencrypt/live/server.maat-bab.com/chain.pem', 'utf8');
-// const ca = fs.readFileSync('/etc/letsencrypt/live/server.maat-bab.com/fullchain.pem', 'utf8');
-
-// const credentials = {
-//   key: key,
-//   cert: cert,
-//   ca: ca,
-// };
-
-// app.use((req, res) => {
-//   res.send('Hello there !');
-// });
-// const httpserver = http.createServer(app);
-// const httpsServer = https.createServer(credentials, app);
-
-// httpserver.listen(3000);
-// httpsServer.listen(443, () => {
-//   console.log('HTTPS Server running on port 443');
-// });
