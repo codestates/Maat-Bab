f13ef0ca86cc3fab3fdf123642d97f0cad628970
zzero0622 <zzero9158@gmail.com>
2021-11-01 15:05:16 +0900

    complete social login by kakao

--- server/controllers/oauth/kakao.js
+++ server/controllers/oauth/kakao.js
@@ -1,36 +1,85 @@
+require('dotenv').config();
+const qs = require('qs');
+const axios = require('axios');
+const { User } = require('../../models');
+const clientID = process.env.KAKAO_clientID;
+const clientSecret = process.env.KAKAO_clientSecret;
+const redirectUri = process.env.KAKAO_redirectUri;
+const { generateAccessToken, generateRefreshToken, sendAccessToken, sendRefreshToken } = require('../tokenFunctions');
+
 module.exports = {
-  get: (req, res) => {
-    console.log(`
-    get kakao
-    params : ${req.params}
-    req : ${req}`);
-    let data = `
-    get kakao
-    params : ${req.params}
-    req : ${req}`;
-    res.status(200).send(`get kakao ${data}`);
-  },
-  post: (req, res) => {},
-  signin: {
-    get: (req, res) => {},
-    post: (req, res) => {},
-  },
-  signout: {},
-  signup: {},
-  token: {
-    get: (req, res) => {},
-    post: (req, res) => {},
-  },
-  userinfo: {
-    get: (req, res) => {},
-  },
-  leave: {
-    get: (req, res) => {},
+  post: async (req, res) => {
+    const code = req.body.code;
+    let token, kakaoUser;
+    try {
+      token = await axios({
+        method: 'POST',
+        url: 'https://kauth.kakao.com/oauth/token',
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+        },
+        data: qs.stringify({
+          //객체를 string 으로 변환
+          grant_type: 'authorization_code', //특정 스트링
+          client_id: clientID,
+          // client_secret: clientSecret,
+          redirectUri: redirectUri,
+          code: code,
+        }),
+      }).then((tokenData) => tokenData.data);
+    } catch (err) {
+      console.log(err);
+      res.status(error.response.status).end();
+    }
+    if (token) {
+      try {
+        kakaoUser = await axios({
+          method: 'get',
+          url: 'https://kapi.kakao.com/v2/user/me',
+          headers: {
+            Authorization: `Bearer ${token.access_token}`,
+          },
+        }).then((kakaoUserData) => kakaoUserData.data);
+      } catch (err) {
+        console.log(err);
+        res.status(error.response.status).end();
+      }
+    }
+
+    if (kakaoUser) {
+      const kakaoId = kakaoUser.id;
+      const name = kakaoUser.kakao_account.profile.nickname;
+      try {
+        await User.findOrCreate({
+          where: { oauth: kakaoId },
+          defaults: {
+            email: null,
+            password: null,
+            name,
+            etiquette: null,
+            oauth: kakaoId,
+            certification: true,
+          },
+        }).then(([result, created]) => {
+          delete result.dataValues.password;
+
+          const userinfo = result.dataValues;
+
+          const accessToken = generateAccessToken(userinfo);
+          const refreshToken = generateRefreshToken(userinfo);
+
+          sendAccessToken(res, accessToken);
+          sendRefreshToken(res, refreshToken);
+
+          if (!created) {
+            return res.status(200).send(userinfo);
+          }
+          return res.status(201).send(userinfo);
+        });
+      } catch (err) {
+        console.log(err);
+        res.status(error.response.status).end();
+      }
+    }
   },
 };
-
-// *client_id: String = 앱 REST API 키 3d5f1fdf916d372e883f1a5a9b993c5f
-// *redirect_uri: String = 인가 코드가 리다이렉트 될 URI
-// *response_type: String = code 로 고정
-// state: String = 로그인 요청과 콜백 간에 상태를 유지하기 위해 사용되는 임의의 문자열(정해진 형식 없음)
-//                 Cross-Site Request Forgery(CSRF) 공격으로부터 보호하기 위해 해당 파라미터 사용을 권장함
