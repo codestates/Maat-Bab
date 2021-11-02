da172b0e5b4f9f301f3ac1de6a16d533d232cbe6
seonwoong <lso5936@gmail.com>
2021-11-02 16:45:31 +0900

    카카오맵 레벨수정

--- client/src/Component/KakaoMap.js
+++ client/src/Component/KakaoMap.js
@@ -25,8 +25,8 @@ const KakaoMap = ({city, district, searchingPlace, setCurnPlace}) => {
     var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 })
     const container = document.getElementById('myMap')
     const options = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667),
-      level: 2,
+      center: new kakao.maps.LatLng(37.566826, 126.570667),
+      level: 3,
     }
     const map = new kakao.maps.Map(container, options);
     const ps = new kakao.maps.services.Places(); 
