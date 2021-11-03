4e6bec1a40b87675779e228a0149ac79abc0eda4
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2021-11-03 21:17:04 +0900

    Modal className 오타수정

--- client/src/Modal/JoinModal.js
+++ client/src/Modal/JoinModal.js
@@ -22,7 +22,7 @@ function JoinModal({ user_id, card_id }) {
             <div className='modalbackdrop join'>
                 <div className='modalview join'>
                     <div className='modalview__massage join'>  맞밥 약속에 참여하시겠습니까?</div>
-                    <div className='modalview__count__massage join'>{`'참여하기'를 누르면
+                    <div className='modalview__count__message join'>{`'참여하기'를 누르면
                         나의 약속 및 채팅 페이지로
                         이동합니다 🏃🏼‍♀️💨`} </div>
                     <button className='modalview__button join' onClick={() => joinHandler(card_id)}>참여하기</button>
