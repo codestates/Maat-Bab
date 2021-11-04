7e1bd41c943d92e0d04bef356f613f84ddbfdb2b
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2021-11-04 17:32:53 +0900

    Chatpage-Card-joined-Delete 구현

--- client/src/Component/Card.js
+++ client/src/Component/Card.js
@@ -4,7 +4,7 @@ import { MdExitToApp } from 'react-icons/md';
 
 function Card({
     card_id, title, region, date, time, headCount,
-    myCard,
+    myCard, selectedCard,
     cardClickinMainHandler, cardClickinChatHandler, deleteCardModalHandler }) {
 
     let cardClickHandler;
@@ -14,13 +14,21 @@ function Card({
         cardClickHandler = cardClickinMainHandler
     }
 
+    let isjoined = '';
+    if (selectedCard.card_id === card_id) {
+        isjoined = 'joined';
+    }
+
     return (
         <div className='card'>
 
             <div className='card__container'>
                 <div className='card__first__container'>
                     <div className='card__title'>{title}</div>
-                    {cardClickinChatHandler? <MdExitToApp className='chat__exit__button' onClick={()=>deleteCardModalHandler()}/> : null}
+                    {cardClickinChatHandler ? (selectedCard.card_id===card_id ? <MdExitToApp className='chat__exit__button joined' onClick={() => deleteCardModalHandler()} />
+                        :
+                        <MdExitToApp className='chat__exit__button' onClick={() => deleteCardModalHandler()} disabled />
+                    ) : null}
                 </div>
             <div className='card__second__container'>
             <span className='card__region'>서울시 {region}</span>
@@ -30,42 +38,11 @@ function Card({
             <span className='card__time'>{time}</span>
             <span className='card__headcount'>{headCount}명</span>
             </div>
-        <div className='card__hover__message' onClick={() => cardClickHandler(myCard)}>채팅하기</div>
+                {selectedCard.card_id===card_id ? <div className='card__hover__message joined' onClick={() => deleteCardModalHandler(myCard)}>나가기</div>
+                    :
+                    <div className='card__hover__message' onClick={() => cardClickHandler(myCard)}>채팅하기</div>
+        }
         </div>
-
-
-        {/* {cardClickinMainHandler && !cardClickinChatHandler ? 
-            <div className='card__container'>
-                <div className='card__first__container'>
-                    <div className='card__title'>{title}</div>
-                </div>
-            <div className='card__second__container'>
-            <span className='card__region'>서울시 {region}</span>
-                <span className='card__date'>{date}</span>
-            </div>
-            <div className='card__third__container'>
-            <span className='card__time'>{time}</span>
-            <span className='card__headcount'>{headCount}명</span>
-            </div>
-        <div className='card__hover__message' onClick={() => cardClickinMainHandler(card_id)}>채팅하기</div>
-        </div>
-        :
-            <div className='card__container'>
-            <div className='card__first__container'>
-                <div className='card__title'>{title}</div>
-                        <MdExitToApp className='chat__exit__button' onClick={()=>deleteCardModalHandler()}/>
-            </div>
-            <div className='card__second__container'>
-            <span className='card__region'>서울시 {region}</span>
-                <span className='card__date'>{date}</span>
-            </div>
-            <div className='card__third__container'>
-            <span className='card__time'>{time}</span>
-            <span className='card__headcount'>{headCount}명</span>
-            </div>
-        <div className='card__hover__message joined' onClick={() => cardClickinChatHandler(card_id)}>채팅하기</div>
-        </div>        
-        } */}
         </div>
     )
 }
--- client/src/Component/CardsList.js
+++ client/src/Component/CardsList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from './Card';
 import './CardsList.css';
 
-function CardsList({ cardData, cardClickinMainHandler, cardClickinChatHandler, deleteCardModalHandler }) {
+function CardsList({ cardData, selectedCard, cardClickinMainHandler, cardClickinChatHandler, deleteCardModalHandler }) {
 
     const isMyCard = cardData?.find(card => card.Card);
 
@@ -27,6 +27,7 @@ function CardsList({ cardData, cardClickinMainHandler, cardClickinChatHandler, d
             {cardList ? cardList?.map(card => {
                     return (
                         <Card className='card__section'
+                            selectedCard={selectedCard}
                             card_id={card.card_id} title={card.chat_title} region={card.region} date={card.date.slice(0, 10)} time={card.time} headCount={card.headcount} restaurant={card.restaurant_name}
                             
                             myCard={card}
