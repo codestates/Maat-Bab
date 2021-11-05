f2defa056c3399d6cacecee4c3f5294ed77336b9
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2021-11-05 15:25:10 +0900

    MateList 수정2

--- client/src/Component/CardsList.js
+++ client/src/Component/CardsList.js
@@ -19,23 +19,28 @@ function CardsList({ cardData, selectedCard, cardClickinMainHandler, cardClickin
         cardList = cardData;
     }
 
+    let nullText = '';
+    if (!cardList) {
+        nullText = '맞밥 약속을 추가해 보세요!'
+    }
+
     return (
         <div className='cardslist'>
             {cardList ? cardList?.map(card => {
-                    return (
-                        <Card className='card__section'
-                            selectedCard={selectedCard}
-                            card_id={card.card_id} title={card.chat_title} region={card.region} date={card.date.slice(0, 10)} time={card.time} headCount={card.headcount} restaurant={card.restaurant_name}
-                            
-                            myCard={card}
-                            cardClickinMainHandler={cardClickinMainHandler}
-                            cardClickinChatHandler={cardClickinChatHandler}
-                            deleteCardModalHandler={deleteCardModalHandler}
-                        />
-                    )
+                return (
+                    <Card className='card__section'
+                        selectedCard={selectedCard}
+                        card_id={card.card_id} title={card.chat_title} region={card.region} date={card.date.slice(0, 10)} time={card.time} headCount={card.headcount} restaurant={card.restaurant_name}
+                        
+                        myCard={card}
+                        cardClickinMainHandler={cardClickinMainHandler}
+                        cardClickinChatHandler={cardClickinChatHandler}
+                        deleteCardModalHandler={deleteCardModalHandler}
+                    />
+                )
             })
                 :
-                <div className='loader__box__ifnull'> 맞밥 약속을 추가해 보세요!</div>
+                <div className='loader__box__ifnull'> {nullText}</div>
             }
         </div>
     )
