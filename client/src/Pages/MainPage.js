9f5f2c7bfb37bf2d96b3045a71e8267e6fa80eb5
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2021-11-05 15:48:26 +0900

    Main-EditInfo datepicker css 수정

--- client/src/Pages/MainPage.js
+++ client/src/Pages/MainPage.js
@@ -7,7 +7,7 @@ import './MainPage.css';
 import axios from 'axios';
 import { getFormatDate1 } from '../functions/module';
 
-function MainPage({ isLogin, userInfo }) {
+function MainPage({ userInfo }) {
   const [curnPlace, setCurnPlace] = useState('');
   console.log(
     'after click pinmarker state curnPlace in MainPage is: ',
@@ -45,15 +45,8 @@ function MainPage({ isLogin, userInfo }) {
   const [clickedCardId, setClickedCardId] = useState(null);
 
   const cardClickinMainHandler = (card) => {
-    console.log('card clicked');
-    // 모달 -> 거기서 참여하기 클릭 시
-    // : 나의 약속에 추가하기
-    // 그런 다음 '/chatpage' 리디렉션
     setClickedCardId(card.card_id);
     setCardClicked(true);
-    console.log(
-      `clickedCardId : ${clickedCardId} isCardClicked ? ${isCardClicked}`
-    );
   };
 
   useEffect(() => {
@@ -71,7 +64,6 @@ function MainPage({ isLogin, userInfo }) {
         setCurnPlace={setCurnPlace}
         curnPlace={curnPlace}
       />
-      {/* 조회된 전체 약속카드 목록 */}
       <List
         className='mainpage__list__component'
         title={'맞밥 약속 목록'}
