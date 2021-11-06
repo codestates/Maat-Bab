f29d8b1e0da1c10d593bf2a44c6fe66eec03ec42
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2021-11-06 19:41:45 +0900

    props rename

--- client/src/Pages/ChatPage.js
+++ client/src/Pages/ChatPage.js
@@ -126,7 +126,7 @@ function ChatPage() {
       {selectedCard ? (
         <ChatBox className='chatpage__chat__container'
           my_user_id={user_id}
-          name={name}
+          my_name={name}
           selectedCard={selectedCard}
           socket={socket}
           isDeleteClicked={isDeleteClicked}
