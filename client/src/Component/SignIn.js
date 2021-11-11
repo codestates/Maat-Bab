d4fc8712829987eb856a741bf6cf2c331cac0963
seonwoong <lso5936@gmail.com>
2021-11-11 10:59:07 +0900

    로그인 alert추가

--- client/src/Component/SignIn.js
+++ client/src/Component/SignIn.js
@@ -47,6 +47,8 @@ function SignIn({ isSiginInModal }) {
           history.push('/main');
         }else if(res.status === 400){
           alert('아이디 또는 비밀번호를 확인해주세요')
+        }else if(res.status === 404){
+          alert('아이디를 확인해주세요')
         }
       });
   };
