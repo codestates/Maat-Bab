eaa9671c19f5346f385f6fff86c31e753927a56d
js5787 <ljs950430@gmail.com>
2021-11-11 19:23:58 +0900

    알림기능클라이언트부분추가

--- client/src/Component/List.js
+++ client/src/Component/List.js
@@ -2,22 +2,38 @@ import React from 'react';
 import CardsList from './CardsList';
 import './List.css';
 
-function List({ title, cardData, message, cardClickinMainHandler, cardClickinChatHandler, setSelectedCard, selectedCard, deleteCardModalHandler
+function List({
+  title,
+  cardData,
+  check_messages,
+  message,
+  cardClickinMainHandler,
+  cardClickinChatHandler,
+  setSelectedCard,
+  selectedCard,
+  deleteCardModalHandler,
 }) {
-    return (
-        <div className='list__backgorund'>
-            <div className='list__header'>
-                <div className='list__header__title'>{title} 🍴</div>
-                <div className='list__header__text'>{message}</div>
-            </div>
-            <section className='card__list__section'>
-                <CardsList
-                    cardData={cardData} selectedCard={selectedCard} setSelectedCard={setSelectedCard}
-                    cardClickinMainHandler={cardClickinMainHandler} cardClickinChatHandler={cardClickinChatHandler} deleteCardModalHandler={deleteCardModalHandler}
-                /> 
-            </section>
-        </div>
-    )
+  return (
+    <div className='list__backgorund'>
+      <div className='list__header'>
+        <div className='list__header__title'>{title} 🍴</div>
+        <div className='list__header__text'>{message}</div>
+      </div>
+      <section className='card__list__section'>
+        <CardsList
+          cardData={cardData}
+          selectedCard={selectedCard}
+          setSelectedCard={setSelectedCard}
+          cardClickinMainHandler={cardClickinMainHandler}
+          cardClickinChatHandler={cardClickinChatHandler}
+          deleteCardModalHandler={deleteCardModalHandler}
+          check_messages={
+            check_messages === null ? check_messages : check_messages
+          }
+        />
+      </section>
+    </div>
+  );
 }
 
-export default List
+export default List;
